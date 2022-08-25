05e086d50d2c7662879a60bc2c205fc3064767bf
Andres Sebastian Martinez Chas <sebastianmartinezchas@gmail.com>
2022-08-25 00:48:29 -0300

    Google Signin added

--- database/config.js
+++ database/config.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbConnection = async () => {
+
   try {
     await mongoose.connect(process.env.MONGODB_CNN);
     console.log("Base de datos online");
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth");
+const { login, googleSignIn } = require("../controllers/auth");
 const { validarCampos } = require("../middlewares/validar-campos");
 const router = Router();
 
@@ -10,4 +10,8 @@ router.post('/login',[
     validarCampos,
 ],login);
 
+router.post('/google',[
+    check('id_token','id_token es necesario').not().isEmpty(),
+    validarCampos,
+],googleSignIn);
 module.exports=router;
